dcfdd6e999d9104ed0f3ed2714bb7998ebf3f761
SauravMishra100 <sm2243@scarletmail.rutgers.edu>
2021-01-09 15:25:37 -0500

    More formatting

--- client/src/Components/Contact/ContactComp/Contact.js
+++ client/src/Components/Contact/ContactComp/Contact.js
@@ -44,14 +44,12 @@ export default class ContactMain extends React.Component {
       );
     }
     return (
-      <div align="center">
-        <p style={{ fontSize: 1.6 + "em" }}>
+        <p className="contactPlaceholder" style={{ fontSize: 1.6 + "em" }}>
           Please contact us at the following:<br></br>
           Office: (609)588-4442<br></br>
           Cell: (609)933-8806<br></br>
           Mail: qlc4044@quaker-bridge.com
         </p>
-      </div>
       
     /*
       <section id="contact1">
