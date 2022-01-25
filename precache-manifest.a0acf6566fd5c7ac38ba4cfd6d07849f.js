1f01a8fb1e50bbf7aaf117f7040637254932fc65
Rohan Deshpande <rohandeshpande832@gmail.com>
2022-01-24 21:44:39 -0500

    Updates

---
+++ precache-manifest.a0acf6566fd5c7ac38ba4cfd6d07849f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb324cbdefd45e9e2532f119d27e9da6",
+    "url": "/qbweb/index.html"
+  },
+  {
+    "revision": "095a25180f07e7ca2328",
+    "url": "/qbweb/static/css/main.dc3f0ce9.chunk.css"
+  },
+  {
+    "revision": "1ccbf6b93fedeeed924e",
+    "url": "/qbweb/static/js/2.47ccebe7.chunk.js"
+  },
+  {
+    "revision": "d0fc412939358a91592f194e64f2710d",
+    "url": "/qbweb/static/js/2.47ccebe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095a25180f07e7ca2328",
+    "url": "/qbweb/static/js/main.b4fc33aa.chunk.js"
+  },
+  {
+    "revision": "1973a86f21c34fa14bb3",
+    "url": "/qbweb/static/js/runtime-main.faed7499.js"
+  },
+  {
+    "revision": "4be86bafeaef5871bca52a374e5e8607",
+    "url": "/qbweb/static/media/banner.4be86baf.png"
+  },
+  {
+    "revision": "0cd9134ed2e3cb5227636ba434785987",
+    "url": "/qbweb/static/media/class.0cd9134e.jpg"
+  },
+  {
+    "revision": "e513536d3b6468f29d10169046bb0d84",
+    "url": "/qbweb/static/media/collegepep.e513536d.jfif"
+  },
+  {
+    "revision": "3c7bb490620d101bb74a56b80deaa25e",
+    "url": "/qbweb/static/media/middle-new.3c7bb490.png"
+  },
+  {
+    "revision": "ac9a2a862de56b647e439b1a80ac116f",
+    "url": "/qbweb/static/media/mit.ac9a2a86.png"
+  },
+  {
+    "revision": "268b9fa53fd3d668366078346d7f5128",
+    "url": "/qbweb/static/media/praxis.268b9fa5.jpg"
+  },
+  {
+    "revision": "07465213b09c195c24a99c7c6ab071c3",
+    "url": "/qbweb/static/media/princeton.07465213.png"
+  },
+  {
+    "revision": "103ff3dc696cd6b150c299a30b507fcb",
+    "url": "/qbweb/static/media/qb.103ff3dc.png"
+  },
+  {
+    "revision": "ef880ddcc1597f81f5670504a1c6bd7d",
+    "url": "/qbweb/static/media/qbhacks.ef880ddc.png"
+  },
+  {
+    "revision": "288ddb8f5d6c6c1f8e98d6e062754030",
+    "url": "/qbweb/static/media/stanford.288ddb8f.png"
+  },
+  {
+    "revision": "eb6efe4c8a338beda4904ac31a54786d",
+    "url": "/qbweb/static/media/students.eb6efe4c.jpg"
+  },
+  {
+    "revision": "2d830e04e5d8fa5c01e676d4711e0df5",
+    "url": "/qbweb/static/media/summer.2d830e04.png"
+  },
+  {
+    "revision": "0fd5daa9c58914383d2cc7778275a474",
+    "url": "/qbweb/static/media/tof-new.0fd5daa9.png"
+  },
+  {
+    "revision": "9f7ac1620f272505bc991cd222809150",
+    "url": "/qbweb/static/media/yale.9f7ac162.png"
+  }
+]);
