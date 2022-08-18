2d4b718ffa1ca78f57a4b3b2215280185673562d
Saurav Mishra <sauravmishra@Sauravs-Air.fios-router.home>
2022-08-18 16:53:48 -0400

    Updates

---
+++ precache-manifest.ba05a8045c540b76fdbf01362afce01a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ed91f79e273a39f243dbba6dd41e31",
+    "url": "https://quaker-bridge.org/index.html"
+  },
+  {
+    "revision": "a16734a5a4be1d749277",
+    "url": "https://quaker-bridge.org/static/css/main.40f0a46c.chunk.css"
+  },
+  {
+    "revision": "dd2c9f716c63a1d01e78",
+    "url": "https://quaker-bridge.org/static/js/2.2cb75011.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "https://quaker-bridge.org/static/js/2.2cb75011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16734a5a4be1d749277",
+    "url": "https://quaker-bridge.org/static/js/main.269046d9.chunk.js"
+  },
+  {
+    "revision": "077098b17c08627133eb",
+    "url": "https://quaker-bridge.org/static/js/runtime-main.37a5f240.js"
+  },
+  {
+    "revision": "4be86bafeaef5871bca52a374e5e8607",
+    "url": "https://quaker-bridge.org/static/media/banner.4be86baf.png"
+  },
+  {
+    "revision": "0cd9134ed2e3cb5227636ba434785987",
+    "url": "https://quaker-bridge.org/static/media/class.0cd9134e.jpg"
+  },
+  {
+    "revision": "e513536d3b6468f29d10169046bb0d84",
+    "url": "https://quaker-bridge.org/static/media/collegepep.e513536d.jfif"
+  },
+  {
+    "revision": "3c7bb490620d101bb74a56b80deaa25e",
+    "url": "https://quaker-bridge.org/static/media/middle-new.3c7bb490.png"
+  },
+  {
+    "revision": "ac9a2a862de56b647e439b1a80ac116f",
+    "url": "https://quaker-bridge.org/static/media/mit.ac9a2a86.png"
+  },
+  {
+    "revision": "268b9fa53fd3d668366078346d7f5128",
+    "url": "https://quaker-bridge.org/static/media/praxis.268b9fa5.jpg"
+  },
+  {
+    "revision": "07465213b09c195c24a99c7c6ab071c3",
+    "url": "https://quaker-bridge.org/static/media/princeton.07465213.png"
+  },
+  {
+    "revision": "103ff3dc696cd6b150c299a30b507fcb",
+    "url": "https://quaker-bridge.org/static/media/qb.103ff3dc.png"
+  },
+  {
+    "revision": "ef880ddcc1597f81f5670504a1c6bd7d",
+    "url": "https://quaker-bridge.org/static/media/qbhacks.ef880ddc.png"
+  },
+  {
+    "revision": "288ddb8f5d6c6c1f8e98d6e062754030",
+    "url": "https://quaker-bridge.org/static/media/stanford.288ddb8f.png"
+  },
+  {
+    "revision": "2d830e04e5d8fa5c01e676d4711e0df5",
+    "url": "https://quaker-bridge.org/static/media/summer.2d830e04.png"
+  },
+  {
+    "revision": "9bd3b9ae4bc2f3cf9ba19ba4f6fa6a2e",
+    "url": "https://quaker-bridge.org/static/media/teachers.9bd3b9ae.jpg"
+  },
+  {
+    "revision": "0fd5daa9c58914383d2cc7778275a474",
+    "url": "https://quaker-bridge.org/static/media/tof-new.0fd5daa9.png"
+  },
+  {
+    "revision": "9f7ac1620f272505bc991cd222809150",
+    "url": "https://quaker-bridge.org/static/media/yale.9f7ac162.png"
+  }
+]);
