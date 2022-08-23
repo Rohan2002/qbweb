743bf5c4083c52b44e21e2dcb2cb889146ae7dc8
Saurav Mishra <sauravmishra@Sauravs-Air.fios-router.home>
2022-08-23 02:56:47 -0400

    Updates

---
+++ precache-manifest.34b2859a5746cfb41e9962a08accf79f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1540395d0383862e3f61d47da823b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44dd0156788c069902af",
+    "url": "/static/css/main.2ee9b4ce.chunk.css"
+  },
+  {
+    "revision": "dd2c9f716c63a1d01e78",
+    "url": "/static/js/2.2cb75011.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/static/js/2.2cb75011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44dd0156788c069902af",
+    "url": "/static/js/main.269046d9.chunk.js"
+  },
+  {
+    "revision": "079d6582fb8c5fc08096",
+    "url": "/static/js/runtime-main.c533b83c.js"
+  },
+  {
+    "revision": "4be86bafeaef5871bca52a374e5e8607",
+    "url": "/static/media/banner.4be86baf.png"
+  },
+  {
+    "revision": "0cd9134ed2e3cb5227636ba434785987",
+    "url": "/static/media/class.0cd9134e.jpg"
+  },
+  {
+    "revision": "e513536d3b6468f29d10169046bb0d84",
+    "url": "/static/media/collegepep.e513536d.jfif"
+  },
+  {
+    "revision": "3c7bb490620d101bb74a56b80deaa25e",
+    "url": "/static/media/middle-new.3c7bb490.png"
+  },
+  {
+    "revision": "ac9a2a862de56b647e439b1a80ac116f",
+    "url": "/static/media/mit.ac9a2a86.png"
+  },
+  {
+    "revision": "268b9fa53fd3d668366078346d7f5128",
+    "url": "/static/media/praxis.268b9fa5.jpg"
+  },
+  {
+    "revision": "07465213b09c195c24a99c7c6ab071c3",
+    "url": "/static/media/princeton.07465213.png"
+  },
+  {
+    "revision": "103ff3dc696cd6b150c299a30b507fcb",
+    "url": "/static/media/qb.103ff3dc.png"
+  },
+  {
+    "revision": "ef880ddcc1597f81f5670504a1c6bd7d",
+    "url": "/static/media/qbhacks.ef880ddc.png"
+  },
+  {
+    "revision": "288ddb8f5d6c6c1f8e98d6e062754030",
+    "url": "/static/media/stanford.288ddb8f.png"
+  },
+  {
+    "revision": "2d830e04e5d8fa5c01e676d4711e0df5",
+    "url": "/static/media/summer.2d830e04.png"
+  },
+  {
+    "revision": "9bd3b9ae4bc2f3cf9ba19ba4f6fa6a2e",
+    "url": "/static/media/teachers.9bd3b9ae.jpg"
+  },
+  {
+    "revision": "0fd5daa9c58914383d2cc7778275a474",
+    "url": "/static/media/tof-new.0fd5daa9.png"
+  },
+  {
+    "revision": "9f7ac1620f272505bc991cd222809150",
+    "url": "/static/media/yale.9f7ac162.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "www.quaker-bridge.org/precache-manifest.efdaccd3068ca0ce27c792fab6b1ea68.js"
+  "/precache-manifest.34b2859a5746cfb41e9962a08accf79f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("www.quaker-bridge.org/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
