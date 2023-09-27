bb054f8ae5807523636f628920e0daf4bff0e8aa
Saurav Mishra <sauravmishra@Sauravs-MacBook-Air.local>
2023-09-27 13:56:33 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c95c796a1386b8339443b5992e30a00d.js"
+  "/precache-manifest.2ee5af3826947ba031eef05a69fe6ad9.js"
 );
 
 self.addEventListener('message', (event) => {
