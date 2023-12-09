c8b3e35e448e66d06c3c15a570b3518db95b1949
Saurav Mishra <sauravmishra@Sauravs-MacBook-Air.local>
2023-12-09 13:22:20 -0500

    Man.

--- src/Components/Home/HomePage/HomePage1.js
+++ src/Components/Home/HomePage/HomePage1.js
@@ -14,7 +14,7 @@ export default class HomePage1 extends React.Component {
           <div className={"message-body"} style={{ marginTop: 3 + "em" }}>
             <Message positive>
               <p style={{ fontSize: 1.6 + "em" }}>
-                Details about the 2024 summer program.<br></br>
+                Details about the 2024 summer program will be released in January.<br></br>
                 For information about the fall program, please click <a href="/WinterCourses2021.pdf">here</a><br></br>
                 To register, please click <a href="/#/register">here</a>.<br></br>
                 For questions and concerns, please click <a href="/#/contact">here</a>.
